02d5541b4d6e54df293dc9344727d24b2d59e30f
Ryan Pineo <ry@tsl.io>
2016-03-22 15:56:32 -0400

    add default pagination of 30

--- settings/base.py
+++ settings/base.py
@@ -141,6 +141,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
