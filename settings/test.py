094cb2dd818143ab53109cae432a0c4a3fa518f5
Ryan Pineo <ry@tsl.io>
2016-07-15 12:14:55 -0400

    uncomment test settings

--- settings/test.py
+++ settings/test.py
@@ -2,10 +2,10 @@ from .dev import *  # noqa
 
 DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
 
-# # Emails
-# DJMAIL_REAL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+# Emails
+DJMAIL_REAL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
-# # Speeds up tests.
-# PASSWORD_HASHERS = [
-#     'django.contrib.auth.hashers.MD5PasswordHasher',
-# ]
+# Speeds up tests.
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
