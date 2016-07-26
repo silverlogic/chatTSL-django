70dfadfadbb65196c47dec274e6c28275f55fe5e
Ryan Pineo <ry@tsl.io>
2016-07-26 16:52:01 -0400

    fix celery autodiscover tasks when app is installed with config

--- apps/celery.py
+++ apps/celery.py
@@ -1,7 +1,11 @@
+import re
+
 from django.conf import settings
 
 from celery import Celery
 
 app = Celery('apps')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# re.sub to remove the `.apps.XConfig` from app entries.
+app.autodiscover_tasks(lambda: [re.sub(r'\.apps\.\w+Config', '', app) for app in settings.INSTALLED_APPS])
