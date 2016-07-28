b379ad6100cbeb48d5b06af8777d6e7f3e3bd58e
Ryan Pineo <ry@tsl.io>
2016-07-28 10:39:49 -0400

    add referral_code to user docs

--- apidocs/helpers/resources.rb
+++ apidocs/helpers/resources.rb
@@ -60,6 +60,7 @@ module Resources
     is_email_confirmed: false,
     new_email: '',
     is_new_email_confirmed: false,
+    referral_code: '81892308alkk&As',
     avatar: {
         full_size: 'https://www.image.com/1.png',
         small: 'https://www.image.com/2.png'
