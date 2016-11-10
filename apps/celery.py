271bcbb65476666739ce468fcbd31f89e9a80a50
Ryan Pineo <ry@tsl.io>
2016-11-10 12:21:11 -0500

    hopefully fix sentry not logging celery error local variables

--- apps/celery.py
+++ apps/celery.py
@@ -2,7 +2,20 @@ import re
 
 from django.conf import settings
 
-from celery import Celery
+import celery
+
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        if hasattr(settings, 'RAVEN_CONFIG') and settings.RAVEN_CONFIG['dsn']:
+            import raven
+            from raven.contrib.celery import (register_signal,
+                                              register_logger_signal)
+
+            client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+            register_logger_signal(client)
+            register_signal(client)
+
 
 app = Celery('apps')
 app.config_from_object('django.conf:settings')
