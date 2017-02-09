de9805909936ba6c3bf853f04494ad98741b08dd
Ryan Pineo <ry@tsl.io>
2017-02-09 11:30:01 -0500

    add phone number support to baseapp

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+from phonenumber_field.modelfields import PhoneNumberField as PhoneNumberModelField
 from rest_framework.serializers import ModelSerializer as OrigModelSerializer
 
-from .fields import ThumbnailImageField
+from .fields import PhoneNumberField, ThumbnailImageField
 
 
 class ModelSerializer(OrigModelSerializer):
@@ -16,5 +17,12 @@ class ModelSerializer(OrigModelSerializer):
         }
         return field_class, field_kwargs
 
+    def build_standard_field(self, field_name, model_field):
+        field_class, field_kwargs = super().build_standard_field(field_name, model_field)
+        if issubclass(field_class, PhoneNumberField) and model_field.blank:
+            field_kwargs['allow_blank'] = True
+        return field_class, field_kwargs
+
 
 ModelSerializer.serializer_field_mapping[models.ImageField] = ThumbnailImageField
+ModelSerializer.serializer_field_mapping[PhoneNumberModelField] = PhoneNumberField
