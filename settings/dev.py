7906b9eee996f197c720c10c413190a739d57983
Matt Galvis <mg@tsl.io>
2017-06-19 11:20:22 -0400

    fixed migrations error with avatar with  INMEMORYSTORAGE

--- settings/dev.py
+++ settings/dev.py
@@ -8,8 +8,6 @@ DEFAULT_FROM_EMAIL = 'john@test.com'
 DJMAIL_REAL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Must be absolute URLs for use in emails.
-DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
-INMEMORYSTORAGE_PERSIST = True
 MEDIA_ROOT = str(BASE_DIR.parent / 'media')
 MEDIA_URL = '{url}/media/'.format(url=URL)
 STATIC_ROOT = str(BASE_DIR.parent / 'static')
