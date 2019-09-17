fdde8e9a67c8d81e36d29d9ad4b4684af54d6c43
Ryan Pineo <ry@tsl.io>
2019-09-17 16:54:21 -0400

    Run black on existing baseapp code

--- apps/urls.py
+++ apps/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('apps.api.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^avatar/', include('avatar.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r"", include("apps.api.urls")),
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    url(r"^avatar/", include("avatar.urls")),
+    url(r"^admin/", admin.site.urls),
 ]
 
 
@@ -15,12 +15,13 @@ if settings.DEBUG:
     import debug_toolbar
     from django.conf.urls.static import static
     from urllib.parse import urlparse
+
     media_url = urlparse(settings.MEDIA_URL)
     static_url = urlparse(settings.STATIC_URL)
     urlpatterns += static(media_url.path, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(static_url.path, document_root=settings.STATIC_ROOT)
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
 
 
-if apps.is_installed('silk'):
-    urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
+if apps.is_installed("silk"):
+    urlpatterns += [url(r"^silk/", include("silk.urls", namespace="silk"))]
--- settings/dev.py
+++ settings/dev.py
@@ -1,32 +1,26 @@
 from .base import *  # noqa
 
 DEBUG = True
-SECRET_KEY = '1234'
+SECRET_KEY = "1234"
 
 # Email
-DEFAULT_FROM_EMAIL = 'john@test.com'
-DJMAIL_REAL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = "john@test.com"
+DJMAIL_REAL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Must be absolute URLs for use in emails.
-MEDIA_ROOT = str(BASE_DIR.parent / 'media')
-MEDIA_URL = '{url}/media/'.format(url=URL)
-STATIC_ROOT = str(BASE_DIR.parent / 'static')
-STATIC_URL = '{url}/static/'.format(url=URL)
+MEDIA_ROOT = str(BASE_DIR.parent / "media")
+MEDIA_URL = "{url}/media/".format(url=URL)
+STATIC_ROOT = str(BASE_DIR.parent / "static")
+STATIC_URL = "{url}/static/".format(url=URL)
 
 # Debug Toolbar
-INSTALLED_APPS += ['debug_toolbar']
-MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
-] + MIDDLEWARE
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': 'apps.base.debug_toolbar.show_toolbar'
-}
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": "apps.base.debug_toolbar.show_toolbar"}
 
 # Silk
-INSTALLED_APPS += ['silk']
-MIDDLEWARE = [
-    'silk.middleware.SilkyMiddleware',
-] + MIDDLEWARE
+INSTALLED_APPS += ["silk"]
+MIDDLEWARE = ["silk.middleware.SilkyMiddleware"] + MIDDLEWARE
 SILKY_PYTHON_PROFILER = True
 SILKY_PYTHON_PROFILER_BINARY = True
 SILKY_META = True
