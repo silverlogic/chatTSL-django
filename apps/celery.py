bac53bc180dc51e59f03ba80c08fb3b35e67e8a1
Rafael Rocha <rr@tsl.io>
2019-10-22 15:25:18 -0300

    update sentry client

--- apps/celery.py
+++ apps/celery.py
@@ -1,18 +1,4 @@
-from django.conf import settings
-
-import celery
-
-
-class Celery(celery.Celery):
-    def on_configure(self):
-        if hasattr(settings, "RAVEN_CONFIG") and settings.RAVEN_CONFIG["dsn"]:
-            import raven
-            from raven.contrib.celery import register_signal, register_logger_signal
-
-            client = raven.Client(settings.RAVEN_CONFIG["dsn"])
-            register_logger_signal(client)
-            register_signal(client)
-
+from celery import Celery
 
 app = Celery("apps")
 app.config_from_object("django.conf:settings", namespace="CELERY")
