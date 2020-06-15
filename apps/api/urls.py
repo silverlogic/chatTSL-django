9ba41cdffb33264c854b6f977d17bd7b61ae3600
SS <ss@tsl.io>
2020-06-16 01:06:19 +0600

    BA-743: django updated to 2.2.13 update with DRF 3.11.0

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import include, url
 
 from .v1.router import router as v1_router
 
-urlpatterns = [url(r"v1/", include(v1_router.urls, namespace="v1"))]
+urlpatterns = [url(r"v1/", include((v1_router.urls, "v1"), namespace="v1"))]
--- apps/api/v1/routers.py
+++ apps/api/v1/routers.py
@@ -1,4 +1,4 @@
-from rest_framework.routers import DefaultRouter, DynamicDetailRoute, DynamicListRoute, Route
+from rest_framework.routers import DefaultRouter, DynamicRoute, Route
 
 
 class DefaultRouter(DefaultRouter):
@@ -11,14 +11,16 @@ class DefaultRouter(DefaultRouter):
             mapping={"get": "list", "post": "create"},
             name="{basename}-list",
             initkwargs={"suffix": "List"},
+            detail=False,
         ),
         # Dynamically generated list routes.
         # Generated using @list_route decorator
         # on methods of the viewset.
-        DynamicListRoute(
-            url=r"^{prefix}/{methodnamehyphen}{trailing_slash}$",
-            name="{basename}-{methodnamehyphen}",
+        DynamicRoute(
+            url=r"^{prefix}/{url_path}{trailing_slash}$",
+            name="{basename}-{url_name}",
             initkwargs={},
+            detail=False,
         ),
         # Detail route.
         Route(
@@ -31,12 +33,14 @@ class DefaultRouter(DefaultRouter):
             },
             name="{basename}-detail",
             initkwargs={"suffix": "Instance"},
+            detail=True,
         ),
         # Dynamically generated detail routes.
         # Generated using @detail_route decorator on methods of the viewset.
-        DynamicDetailRoute(
-            url=r"^{prefix}/{lookup}/{methodnamehyphen}{trailing_slash}$",
-            name="{basename}-{methodnamehyphen}",
+        DynamicRoute(
+            url=r"^{prefix}/{lookup}/{url_path}{trailing_slash}$",
+            name="{basename}-{url_name}",
             initkwargs={},
+            detail=True,
         ),
     ]
